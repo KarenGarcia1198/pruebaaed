4a57f1b0fe2981932570cd1de9086e58f2739418
Karen Garcia <kagafa_karen@hotmail.com>
2021-04-23 22:18:52 -0500

    create a 3 variables and create ggetters and setters, finally create to string

---
+++ text-prueba1/src/ingresodatos.java
@@ -0,0 +1,37 @@
+
+public class ingresodatos {
+	private int num;
+	private String nombre;
+	private String Apellido;
+	public ingresodatos(int num, String nombre, String apellido) {
+		super();
+		this.num = num;
+		this.nombre = nombre;
+		Apellido = apellido;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	@Override
+	public String toString() {
+		return "ingresodatos [num=" + num + ", nombre=" + nombre + ", Apellido=" + Apellido + "]";
+	}
+	
+	
+
+}
